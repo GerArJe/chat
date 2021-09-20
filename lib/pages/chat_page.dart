68fde6814a3e1609725179b1dabb419886d04da1
German <germanarevalojerez@gmail.com>
2021-09-20 14:54:04 -0500

    chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,14 +1,103 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
+  final _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text('Te', style: TextStyle(fontSize: 12)),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Text('Melissa Flores',
+                style: TextStyle(color: Colors.black87, fontSize: 12)),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+                child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (_, i) => Text('$i'),
+              reverse: true,
+            )),
+            Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: _inputChat(),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String text) {},
+                decoration: InputDecoration.collapsed(hintText: 'Send'),
+                focusNode: _focusNode,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Platform.isIOS
+                  ? CupertinoButton(
+                      child: Text('Send'),
+                      onPressed: () {},
+                    )
+                  : Container(
+                      margin: EdgeInsets.symmetric(horizontal: 4.0),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.send, color: Colors.blue[400])),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _handleSubmit(String text) {
+    print(text);
+    _textController.clear();
+    _focusNode.requestFocus();
+
   }
 }
